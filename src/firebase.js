67f713c2d8ade56abff1f4ee2b58db70c227b377
Alex Zongo <alexzongo@Alexs-MacBook-Pro.local>
2021-01-17 15:37:31 +0800

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCRibiwXrmAhT4tDlM98LKIO51QREdAmE",
+  authDomain: "whatsapp-clone-e3f5e.firebaseapp.com",
+  projectId: "whatsapp-clone-e3f5e",
+  storageBucket: "whatsapp-clone-e3f5e.appspot.com",
+  messagingSenderId: "746006515742",
+  appId: "1:746006515742:web:38066cc8659597442c9e0f",
+  measurementId: "G-M11Q70MEQZ",
+};
